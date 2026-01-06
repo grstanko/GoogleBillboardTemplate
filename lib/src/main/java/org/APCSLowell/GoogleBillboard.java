71fe36604ccbafd9cbc5bf9fa717fb0d33303564
grstanko <144853612+grstanko@users.noreply.github.com>
2026-01-06 20:15:25 +0000

    error!

--- lib/src/main/java/org/APCSLowell/GoogleBillboard.java
+++ lib/src/main/java/org/APCSLowell/GoogleBillboard.java
@@ -14,7 +14,7 @@ public class GoogleBillboard {
                 return false;
             }
         }
-        return true;
+        return true
     }
     public long firstPrimeNumberIn(String n) {
         for (int i = 0; i  <= n.length() - 10; i++) {
